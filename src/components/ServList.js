10d361f869702b7914cd70a8daaa20329715e98c
Vera957 <good.answer.e@gmail.com>
2023-02-14 23:20:53 +0200

    fix

--- src/components/ServList.js
+++ src/components/ServList.js
@@ -5,5 +5,5 @@ export const servList = [
     { name: 'Земельні спори', discription: '', img: 'https://live.staticflickr.com/65535/52688675947_a20dfbcaf8_q.jpg' },
     { name: 'Договірні правовідносини', discription: '', img: 'https://live.staticflickr.com/65535/52689200336_421afa82ee_q.jpg' },
     { name: 'Справи про адміністративні правопорушення', discription: '', img: 'https://live.staticflickr.com/65535/52689470539_ac4b2dfcb0_q.jpg' },
-    { name: 'Представництво інтересів потерпілих у кримінальному провадженні', discription: 'https://live.staticflickr.com/65535/52689689723_d379dd4077_q.jpg' },
+    { name: 'Представництво інтересів потерпілих у кримінальному провадженні', discription: '', img: 'https://live.staticflickr.com/65535/52689689723_d379dd4077_q.jpg' },
 ]
